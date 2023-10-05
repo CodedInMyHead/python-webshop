275838821bf21c4f2b8415e9398e91720f9b84d8
Micha S <empty>
2023-10-05 10:22:14 +0200

    refactor

--- app.py
+++ app.py
@@ -1,14 +1,28 @@
 from flask import Flask, render_template
 
-from sqlalchemy import create_engine, ForeignKey, String, Integer, Column, VARCHAR, CHAR, null
+from sqlalchemy import create_engine, String, Integer, Column, CHAR
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-app = Flask(__name__)
-Base = declarative_base()
+
+def create_app():
+    app_flask = Flask(__name__)
+    base_var = declarative_base()
+    return app_flask, base_var
 
 
-class Person(Base):
+def create_engines():
+    engine = create_engine('sqlite:///data.db')
+    base.metadata.create_all(bind=engine)
+    sm = sessionmaker(bind=engine)
+    session = sm()
+    return session
+
+
+app, base = create_app()
+
+
+class Person(base):
     __tablename__ = "people"
     ssn = Column("ssn", Integer, primary_key=True)
     firstname = Column("firstname", String)
@@ -27,7 +41,7 @@ class Person(Base):
         return f"({self.ssn} {self.first} {self.last} {self.gender} {self.age})"
 
 
-class Util(Base):
+class Util(base):
     __tablename__ = "util"
     ssn = Column("ssn", Integer, primary_key=True)
     person_counter = Column("person_counter", Integer)
@@ -40,39 +54,35 @@ class Util(Base):
         return f"({self.ssn} {self.person_counter})"
 
 
-engine = create_engine('sqlite:///data.db')
-Base.metadata.create_all(bind=engine)
+sqlite_session = create_engines()
 
-Session = sessionmaker(bind=engine)
-session = Session()
-
-util = session.query(Util).filter_by(ssn=1).first()
+util = sqlite_session.query(Util).filter_by(ssn=1).first()
 if util is None:
     util = Util(1, 0)
-    session.add(util)
-    session.commit()
+    sqlite_session.add(util)
+    sqlite_session.commit()
 
 
 @app.route("/api/v1/user", methods=['POST'])
 def add_user():
     person = Person(util.person_counter, "Micha", "Schmitt", "m", 20)
     util.person_counter = util.person_counter + 1
-    session.add(person)
-    to_update = session.query(Util).filter_by(ssn=1).first()
+    sqlite_session.add(person)
+    to_update = sqlite_session.query(Util).filter_by(ssn=1).first()
     to_update.person_counter = util.person_counter
-    session.commit()
+    sqlite_session.commit()
     return ""
 
 
 @app.route("/api/v1/user/<int:user_id>", methods=['GET'])
 def get_user(user_id):
-    person = session.query(Person).filter(Person.ssn == user_id)
+    person = sqlite_session.query(Person).filter(Person.ssn == user_id)
     return f"<p>{person[0].firstname} {person[0].lastname}'s gender is {person[0].gender} and he is {person[0].age} years old.</p>"
 
 
 @app.route("/api/v1/user", methods=['GET'])
 def get_users():
-    people = session.query(Person)
+    people = sqlite_session.query(Person)
     result = ""
     for person in people:
         result = result + f"<p>{person.firstname} {person.lastname}'s gender is {person.gender} and he is {person.age} years old. His ID is {person.ssn}</p>"
